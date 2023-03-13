74767b8276b749c73cad0cea69f856e55f4bb09b
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2023-03-14 04:26:01 +0600

    first tryout

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.use("/", async (req: Request, res: Response) => {
+  return res.status(200).json("hello world");
+});
+app.listen(PORT, () => {
+  console.log(`Bot listening on port ${PORT}`);
+});
